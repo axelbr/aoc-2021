9b4ee958b6955b6a0862c9929c35891496856847
Axel Brunnbauer <axel.brunnbauer@gmx.at>
2021-12-06 17:11:10 +0000

    refactor function

--- aoc/day5/ventures.jl
+++ aoc/day5/ventures.jl
@@ -22,19 +22,25 @@ function draw_map(line_segments)
     for i in 1:2:size(line_segments)[1]
         x1, y1 = line_segments[i, 1] - min + 1, line_segments[i, 2] - min + 1
         x2, y2 = line_segments[i+1, 1] - min + 1, line_segments[i+1, 2] - min + 1
-        x_step = x1 < x2 ? 1 : -1
-        y_step = y1 < y2 ? 1 : -1
-        if x1 == x2
-            field[y1:y_step:y2, x1] = field[y1:y_step:y2, x1] .+ 1
-        elseif y1 == y2
-            field[y1, x1:x_step:x2] = field[y1, x1:x_step:x2] .+ 1
-        elseif abs(x1-x2) == abs(y1-y2)
-            for (x, y) in zip(x1:x_step:x2, y1:y_step:y2)
-                field[y, x] += 1
-            end
-        end
+        draw_line(field, (x1, y1), (x2, y2)) 
     end
     return field
+end
+
+function draw_line(field, p1, p2)
+    x1, y1 = p1
+    x2, y2 = p2
+    x_step = x1 < x2 ? 1 : -1
+    y_step = y1 < y2 ? 1 : -1
+    if x1 == x2
+        field[y1:y_step:y2, x1] = field[y1:y_step:y2, x1] .+ 1
+    elseif y1 == y2
+        field[y1, x1:x_step:x2] = field[y1, x1:x_step:x2] .+ 1
+    elseif abs(x1-x2) == abs(y1-y2)
+        for (x, y) in zip(x1:x_step:x2, y1:y_step:y2)
+            field[y, x] += 1
+        end
+    end
 end
 
 function find_dangerous_areas(map)

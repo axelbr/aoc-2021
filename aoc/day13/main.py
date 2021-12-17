a86f642bedb16691badf1e83b7b6a6884b9cc480
Axel Brunnbauer <axel.brunnbauer@tuwien.ac.at>
2021-12-17 22:06:13 +0100

    finish day 13

--- aoc/day13/main.py
+++ aoc/day13/main.py
@@ -18,7 +18,7 @@ def load_problem(path: str) -> Tuple[np.ndarray, List[Tuple[str, int]]]:
                 line = line.replace('fold along ', '')
                 instructions.append((line.split('=')[0], int(line.split('=')[1])))
         coordinates = np.array(coordinates)
-        sheet = np.zeros((np.max(coordinates[:, 1])+1, np.max(coordinates[:, 0])+1))
+        sheet = np.zeros((np.max(coordinates[:, 1])+1, np.max(coordinates[:, 0])+1), dtype=int)
         for c in coordinates:
             sheet[c[1], c[0]] = 1
         return sheet, instructions
@@ -33,11 +33,16 @@ def fold(sheet: np.ndarray, instructions: List[Tuple[str, int]]) -> np.ndarray:
     return np.minimum(sheet, 1)
 
 def main(args):
-    sheet, instructions = load_problem('./input/small.txt')
-    sheet = fold(sheet, instructions)
-    print(sheet)
-    print(instructions)
-    print(sheet.sum())
+    sheet, instructions = load_problem('./input/large.txt')
+    folded = fold(sheet, instructions[:1])
+    print(f'Part 1 | Number of dots: {folded.sum()}')
+    print(f'Part 2 | Folded sheet: ')
+    folded = fold(sheet, instructions)
+    with np.printoptions(linewidth=1000):
+        code = np.zeros_like(folded, dtype=str)
+        code[folded == 1] = '#'
+        code[folded == 0] = ' '
+        print(code)
 
 if __name__ == '__main__':
     main(sys.argv)
